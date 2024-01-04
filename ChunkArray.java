95cd285871ff7555d3d1a8be85a05a41b52bcf1d
ayushman sethy <ayush15chinku@gamil.com>
2024-01-04 12:18:20 +0530

    "added printing even odd

--- ChunkArray.java
+++ ChunkArray.java
@@ -1,4 +1,3 @@
-package test;
 /**
  * ChunkArray
  */
---
+++ PrintEvenOdd.java
@@ -0,0 +1,69 @@
+
+public class PrintEvenOdd {
+
+    public static void main(String... args) {
+    Printer print = new Printer();
+    Thread t1 = new Thread(new TaskEvenOdd(print, 10, false));
+    Thread t2 = new Thread(new TaskEvenOdd(print, 10, true));
+    t1.start();
+    t2.start();
+}
+}
+
+ class TaskEvenOdd implements Runnable {
+    private int max;
+    private Printer print;
+    private boolean isEvenNumber;
+
+    
+
+    public TaskEvenOdd( Printer print,int max, boolean isEvenNumber) {
+        this.max = max;
+        this.print = print;
+        this.isEvenNumber = isEvenNumber;
+    }
+
+
+
+    @Override
+    public void run() {
+        int number = isEvenNumber ? 2 : 1;
+        while (number <= max) {
+            if (isEvenNumber) {
+                print.printEven(number);
+            } else {
+                print.printOdd(number);
+            }
+            number += 2;
+        }
+    }
+}
+class Printer {
+    private volatile boolean isOdd;
+
+    synchronized void printEven(int number) {
+        while (!isOdd) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println( number);
+        isOdd = false;
+        notify();
+    }
+
+    synchronized void printOdd(int number) {
+        while (isOdd) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println(number);
+        isOdd = true;
+        notify();
+    }
+}
